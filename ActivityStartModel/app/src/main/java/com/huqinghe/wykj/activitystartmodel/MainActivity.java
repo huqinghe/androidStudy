a7013a5231f1abd6b9f9b44a4331f1d6eb9b9dea
huqinghe <huqinghe@weiyankeji.cn>
2017-11-27 21:26:37 +0800

    Activty启动模式
    Activty启动模式

---
+++ ActivityStartModel/app/src/main/java/com/huqinghe/wykj/activitystartmodel/MainActivity.java
@@ -0,0 +1,27 @@
+package com.huqinghe.wykj.activitystartmodel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("myTag===:", this.toString());
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
